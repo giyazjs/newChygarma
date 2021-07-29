d8527c20a730a9ae5073cca13dee6798629bbe5e
Giyaz <atazhanovgiyaz@gmail.com>
2021-07-29 21:49:18 +0600

    file

--- file.js
+++ file.js
@@ -1,10 +1,9 @@
-const input = document.querySelector('.input__file');
+const input = document.querySelector('#input__file');
 const btnbg = document.querySelector('#input__filed');
-const btncom = document.querySelector('.input__file-com')
-
-
-
-
+const btncom = document.querySelector('#input__file-com')
+const block = document.querySelector('#com-bg')
+const bgavatar = document.getElementById("bg-avatar")
+const avatar = document.getElementById("avatar")
 
 
 input.onchange = (e) => {
@@ -17,8 +16,8 @@ function readURL(input) {
         var reader = new FileReader();
         reader.onload = function (e) {
             console.log(e.target.result);
-            document.getElementById("avatar").style.background = `url(${e.target.result})`;
-            document.getElementById("avatar").style.backgroundSize = `cover`;
+            avatar.style.background = `url(${e.target.result})`;
+            avatar.style.backgroundSize = `cover`;
         };
 
         reader.readAsDataURL(input.files[0]);
@@ -28,17 +27,17 @@ function readURL(input) {
 
 
 btnbg.onchange = (e) => {
-    readURL1(e.target)
+    readURLbg(e.target)
 }
 
 
-function readURL1(btnbg) {
+function readURLbg(btnbg) {
     if (btnbg.files && btnbg.files[0]) {
         let other = new FileReader();
         other.onload = function (e) {
             // console.log(e.target.result);
-            document.getElementById("bg-avatar").style.background = `url(${e.target.result})`;
-            document.getElementById("bg-avatar").style.backgroundSize = `cover`;
+            bgavatar.style.background = `url(${e.target.result})`;
+            bgavatar.style.backgroundSize = `cover`;
         };
 
         other.readAsDataURL(btnbg.files[0]);
@@ -47,34 +46,21 @@ function readURL1(btnbg) {
 
 
 
-// btncom.onchange = (e) => {
-//     readURL(e.target)
-// }
-
-// btncom.addEventListener('click', function() {
-//     console.log(btncom);
-// })
+btncom.onchange = (e) => {
+    readURLcom(e.target)
+}
 
 
-// function readURL(btncom) {
-//     if (btncom.files && btncom.files[0]) {
-//         var old = new FileReader();
-//         old.onload = function (e) {
-//             // console.log(e.target.result);
-//             document.getElementById("com-bg").style.background = `url(${e.target.result})`;
-//             document.getElementById("com-bg").style.backgroundSize = `cover`;
-//         };
+function readURLcom(btncom) {
+    if (btncom.files && btncom.files[0]) {
+        let old = new FileReader();
+        old.onload = function (e) {
+            // console.log(e.target.result);
+            block.style.background = `url(${e.target.result})`;
+            block.style.backgroundSize = `cover`;
+        };
 
-//         old.readAsDataURL(btncom.files[0]);
-//     }
-// }
+        old.readAsDataURL(btncom.files[0]);
+    }
+}
 
-
-
-
-
-
-
-// btnbg.addEventListener('click', function() {
-    
-// })
