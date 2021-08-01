fe6c6b854d752ac9e6428b43995a41b6b94f6441
Giyaz <atazhanovgiyaz@gmail.com>
2021-08-01 07:46:19 +0600

    add slick-slider all

--- main.js
+++ main.js
@@ -1,16 +1,49 @@
 
-$('.comments__blocks').slick ({
-    slidesToShow: 4,
+$('.slick-block').slick ({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1000,
+})
+
+$('.creations__title').slick ({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1000,
+})
+
+$('.all__content').slick ({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1000,
+})
+
+$('.populars__slider').slick ({
+    slidesToShow: 3,
     slidesToScroll: 1,
     responsive: [
         {
-            breakpoint: 769,
+            breakpoint: 769,    
             settings: {
-                slidesToShow: 2
+                slidesToShow: 2    
             }
         }
     ]
 })
+
+// $('.comments__blocks').slick ({
+//     slidesToShow: 4,    
+//     slidesToScroll: 1,
+//     responsive: [
+//         {
+//             breakpoint: 769,    
+//             settings: {
+//                 slidesToShow: 2    
+//             }
+//         }
+//     ]
+// })
+
+
 
 
 
